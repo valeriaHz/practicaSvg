3a43cc8f533d9f15d85107fb7b4bbecb35d426ad
valer <valeriahernandez063@gmail.com>
2022-11-27 19:46:57 -0600

    https://github.com/valeriaHz/practicaSvg.git

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- manda a traer la hoja de estilos -->
+    <link rel="stylesheet" href="./css/estilos.css">
+    <style>
+        body{
+            /* le da color al fondo */
+            background-color:goldenrod;
+        }
+    </style>
+    <title>SVG</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php include "./header.php"; ?>
+
+<div class="circulo">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+            x="0px" y="0px" viewBox="0 0 1920 1080" style="enable-background:new 0 0 1920 1080;" 
+            xml:space="preserve">
+            <!-- le da estilo al circulo delimitando el ancho 
+                    y el color de la linea, así como el color de relleno 
+                    y la cantidad del circulo que resaltara-->
+        <style type="text/css">
+            .st0 {
+                fill: #FFFFFF;
+            }
+
+            .st1 {
+                fill: #FFFFFF;
+                stroke: #000000;
+                stroke-miterlimit: 10;
+            }
+
+            .st2 {
+                fill: #F700F7;
+                stroke: #000000;
+                stroke-miterlimit: 10;
+            }
+        </style>
+
+        <!-- se divide por capas ya que es una imagen vectorial -->
+        <g id="Capa_1">
+            <g>
+                <!-- en la etiqueta a xlink nos permite mostrar el titulo en la parte
+                    del circulo que no esta sombreada y el xlink href nos da un link al que nos mandara
+                    en este caso se le asigna # ya que no nos mandara a ningun sitio
+                    target nos permite redireccionar el link en una pestaña nueva -->
+                <a xlink:title="1" xlink:href="#" target="blank">
+                    <path class="st0" d="M875.5,830.5c-98.3,0-200.3-39.1-279.8-107.4c-79-67.8-125.8-155.3-128.2-240.1
+			c-1.3-44.8,9.6-90.2,32.4-134.9c18.3-36,39.8-62.1,49-73.3c45.5-55.5,105.8-95.3,179.2-118.4c58.5-18.4,109.9-21.2,135.3-21.2
+			c4.3,0,8.4,0.1,12.1,0.2c92.7,3.9,194.1,41.3,271.4,99.9c41.8,31.8,74.9,68.1,98.1,108.1c25.5,43.8,38.5,90.8,38.5,139.5
+			c0,46.9-10.8,92.4-32,135.2c-20.5,41.4-50,78.5-87.4,110.5c-37.5,31.9-81.1,57-129.7,74.5C984,821.3,930.6,830.5,875.5,830.5z" />
+                </a>
+                <path d="M863.4,134.8v1c4.3,0,8.4,0.1,12,0.2c92.6,3.9,193.9,41.2,271.1,99.8c41.8,31.7,74.8,68,98,107.9
+			c25.5,43.8,38.4,90.6,38.4,139.3c0,46.8-10.8,92.2-32,135c-20.5,41.3-49.9,78.4-87.3,110.3c-37.4,31.9-81,56.9-129.5,74.4
+			C983.9,820.8,930.5,830,875.5,830c-50.3,0-100.8-9.9-150.3-29.4c-46.5-18.3-91.2-45.3-129.2-77.9C517.1,655,470.4,567.6,468,483
+			c-1.3-44.7,9.6-90,32.4-134.7c18.3-35.9,39.7-62,48.9-73.2c24.6-30,53.9-55.7,87.2-76.4c27.8-17.4,58.7-31.4,91.8-41.8
+			c58.4-18.4,109.8-21.1,135.2-21.1V134.8 M863.4,134.8c-48,0-208.3,9.9-314.9,139.7C529.5,297.7,464,377.4,467,483
+			c5.1,177.3,201,348,408.5,348c225.6,0,408.5-155.8,408.5-348c0-204.9-228.6-340.4-408.5-348C873.2,134.9,869.1,134.8,863.4,134.8
+			L863.4,134.8z" />
+            </g>
+            <!-- line crea lineas rectas con las coordenadas en (x1,y1) y (x2,y2) -->
+            <line class="st1" x1="875.5" y1="135" x2="875.5" y2="831" />
+            <path class="st1" d="M466.5,479.5c0,1.7,0,3,0,3S466.5,481.2,466.5,479.5s0-3,0-3S466.5,477.8,466.5,479.5z" />
+            <path class="st1" d="M466.5,479.5" />
+        </g>
+        <g id="Capa_2">
+            <!-- crea la linea del circulo -->
+            <polyline class="st1" points="1284,483 875.5,483 466.5,479.5 	" />
+        </g>
+        <g id="Capa_3">
+            <!-- xlink permite visualizar el nombre del titulo al poner el cursor 
+                sobre esa parte de la imagen -->
+            <a xlink:title="titulo" xlink:href="" target="blank">
+                <path class="st1" d="M875.5,134.5" />
+                <path class="st2" d="M863.2,135c6.6-0.5,12.2-0.8,12.2-0.5c0.1,1.2-98-3.2-191,36c-15.1,6.3-77.1,33.7-134,96
+		c-27.3,29.9-73.8,80.9-83.6,158.2c-2.9,22.7-1.8,41.8-0.4,54.8c136.3,1,272.7,2,409,3c-1.9-53.5-5.1-146.5-7-200
+		C867.4,250.6,864.4,166.9,863.2,135z" />
+            </a>
+        </g>
+    </svg>
+</div>
+
+<?php include "./footer.php"; ?>
